6c3efc0c8f92a4b274ab3777fb0956b1c6a09d9b
Mst. Nazneen Aktar <136933385+Web-and-Cloud@users.noreply.github.com>
2023-06-19 02:49:51 +0300

    Add files via upload

---
+++ navigation/sticky.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', function(){
+
+    let navbar = this.document.getElementById('menu-bar');
+
+    if(this.window.pageYOffset >= 411){
+        navbar.classList.add('sticky');
+    }else{
+        navbar.classList.remove('sticky');
+    }
+
+})
